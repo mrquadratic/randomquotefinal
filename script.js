aa8f64b739366435ca1490c1175846e86a9ca5ac
Jake <zamorejake@gmail.com>
2025-12-11 21:00:13 -0600

    fixed JS quotes and added html/css

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // Jake Zamarripa Final 12/11/25
 
+let oldQuote = "";
 
 function generateQuote() {
-    let intro = ["Sometimes the", "That's how the", "Don't let the"];
-    let subject = ["Turkey", "Polar Bear", "Iguana"];
-    let outro = ["Gobbles", "Chills", "Touch you"];
+    let intro = ["Sometimes in life a", "That's how the", "A mysterious"];
+    let subject = ["Turkey", "Polar Bear", "Gorilla"];
+    let outro = ["gobbles", "chills", "examines the economy"];
 
     // I know this is sloppy but it works
-    let picker = Math.floor(Math.random() * 2) + 1;
-    let picker2 = Math.floor(Math.random() * 2) + 1;
-    let picker3 = Math.floor(Math.random() * 2) + 1;
-
+    let picker = Math.floor(Math.random() * 3);
+    let picker2 = Math.floor(Math.random() * 3);
+    let picker3 = Math.floor(Math.random() * 3);
     let quote = `${intro[picker]} ${subject[picker2]} ${outro[picker3]}!`;
-
+    return quote
 }
+
+function checkQuote() {
+    let newQuote = generateQuote();
+    
+    while (newQuote === oldQuote) {
+        newQuote = generateQuote();
+    }
+
+    document.getElementById("quoteText").textContent = newQuote;
+
+    oldQuote = newQuote
+}
+
+document.getElementById("quoteButton").addEventListener("click", checkQuote)
